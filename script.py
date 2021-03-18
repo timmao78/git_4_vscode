c00bc614a35eb9b9ba451456343dd74d41dc36c4
Tim Mao <tim.mao@gmail.com>
2021-03-19 09:31:15 +1300

    Moved some code

--- script.py
+++ script.py
@@ -1,22 +1,5 @@
-import sys
-
 import requests
 
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = 'Hello, {}'.format(who_to_greet)
-    return greeting
-
-
-# print(greet('World'))
-# print(greet('Tim'))
 
 r = requests.get("http://coreyms.com")
 print(r.status_code)
-
-
-name = input("your name? ")
-print("Hello,", name)
